e10cc9fbb9f92c416cd24a95f2558429c445a91a
Elske <e.muijderman@gmail.com>
2020-02-07 13:00:12 +0100

    här vi kör igen

---
+++ elerif/src/main/java/com/yrgo/contactbook/App.java
@@ -0,0 +1,13 @@
+package com.yrgo.contactbook;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
