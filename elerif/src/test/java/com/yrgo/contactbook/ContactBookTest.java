72c1a3cbef87ff59f8faa719fa14570627498fa9
Elske <e.muijderman@gmail.com>
2020-02-09 21:40:08 +0100

    Skrev några test till

--- elerif/src/test/java/com/yrgo/contactbook/ContactBookTest.java
+++ elerif/src/test/java/com/yrgo/contactbook/ContactBookTest.java
@@ -1,10 +1,7 @@
 package com.yrgo.contactbook;
 
-import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
-
-import java.util.ArrayList;
-import java.util.List;
 
 import org.junit.Test;
 
@@ -13,13 +10,11 @@ import domän.ContactBook;
 
 public class ContactBookTest {
 	
-//	private List<Contact> contacts;
 	private Contact c;
 	private ContactBook cl;
 	
 	public ContactBookTest() {
 		c = new Contact();
-//		contacts = new ArrayList<>();
 		cl = new ContactBook();
 	}
 	
@@ -28,18 +23,29 @@ public class ContactBookTest {
 	public void addContactTest() {
 		ContactBookTest bt = new ContactBookTest();
 		boolean sizeContacts = cl.numberOfContacts() == 0;
+		
 		assertTrue("Arraylisten cl är tom", sizeContacts);
 		
 		c = new Contact("Sara", "saar@mejl.se", "073 45 67 89");
 		cl.addContactToList(c.createContact("Sara", "saar@mejl.se", "073 45 67 89"));
 		boolean sizeAfterAdd = cl.numberOfContacts() == 1;
+		
 		assertTrue("Kontakten finns i listan, listan är inte tom mer", sizeAfterAdd);
 	}
 	
 	@Test
 	public void findContactTest() {
+		ContactBookTest bt = new ContactBookTest();
+		c = new Contact("Sara", "saar@mejl.se", "073 45 67 89");
+		cl.addContactToList(c.createContact("Sara", "saar@mejl.se", "073 45 67 89"));
 		
+		boolean contactFound = cl.findContact("sara").equalsIgnoreCase("Följande kontakt hittades: \n"+ c.toString());
+		
+		assertTrue("Korrekta kontakt hittades", contactFound);
 	}
 	
+	public void updateContactTest() {
+		
+	}
 
 }
