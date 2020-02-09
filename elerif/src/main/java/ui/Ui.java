c50f01b1b9713729519486a7edb11b0b2e855831
Aoife Tjärnlund <tjarnlunda@gmail.com>
2020-02-09 13:49:47 +0100

    GUI är inte färdigt puss

--- elerif/src/main/java/ui/Ui.java
+++ elerif/src/main/java/ui/Ui.java
@@ -1,50 +1,68 @@
 package ui;
 
+import java.awt.FlowLayout;
 import java.awt.GridLayout;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-public class Ui {
+import domän.Contact;
+import domän.ContactBook;
+
+
+public class Ui extends JFrame{
 	
 	private JFrame frame;
-	private JLabel rubrik;
 	private JLabel status;
 	private JPanel panel;
-
-	public static void main(String[] args) {
+	String name = Contact.getNamn();
 	
-
-	}
 	
 	private void userInterface() {
-		frame = new JFrame("Contacts");
+		frame = new JFrame("Contact book");
 		frame.setSize(400, 400);
-		frame.setLayout(new GridLayout());
+		frame.setLayout(new GridLayout(3, 1));
 		
-		frame.addWindowListener(new WindowAdapter() {
-			public void windowClosing(WindowEvent windowEvent) {
-				System.exit(0);
-				
-			}
-			
-		});
+		status = new JLabel("", JLabel.CENTER);
+		status.setSize(350, 100);
+		frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
 		
-	}
+		panel = new JPanel();
+		panel.setLayout(new FlowLayout());
+		
+		frame.add(panel);
+		frame.add(status);
+		frame.setVisible(true);
+		
+		}
 	
-	public void buttons() {
+	public void showEvent() {
+		
 		JButton addContact = new JButton("Add contact");
 		JButton updateContact = new JButton("Update contact");
 		JButton deleteContact = new JButton("Delete contact");
-		JButton showList = new JButton("Show contacts");
-
+		JButton searchContact = new JButton("Search contacts");
+		JButton showList = new JButton("Show contact list");
 		
+		addContact.setActionCommand(null);
+		updateContact.setActionCommand(null);
+		deleteContact.setActionCommand(null);
+		searchContact.setActionCommand(null);
+		showList.setActionCommand(null);
+		
+		panel.add(addContact);
+		panel.add(updateContact);
+		panel.add(deleteContact);
+		panel.add(showList);
+		
+	}
+	public void buttonAction() {
 		
 	}
 	
-
+		
+	
 }
+
