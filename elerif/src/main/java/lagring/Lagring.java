48302b949d07f0c4b40f2fb864470f9330ae3941
plidoodo <blixtdackhammar@gmail.com>
2020-02-10 10:57:48 +0100

    skapa addContactToFile-metod

--- elerif/src/main/java/lagring/Lagring.java
+++ elerif/src/main/java/lagring/Lagring.java
@@ -1,5 +1,40 @@
 package lagring;
 
-public class Lagring {
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
+import domän.Contact;
+import domän.ContactBook;
+
+public class Lagring {
+	
+	ContactBook cb = new ContactBook();
+	
+
+	
+	static File contacts = new File("src\\main\\resources\\contacts.txt");
+	
+	private static void addContactToFile(Contact contact) {
+			
+		
+		try (BufferedWriter writer = new BufferedWriter(new FileWriter(contacts))) {
+			writer.write(contact.getNamn() + "\r\n" + contact.getMejl() + "\r\n" + contact.getTelefonNr() + "\r\n\r\n");
+		} catch (IOException e) {
+			
+			e.printStackTrace();
+		}
+		
+		}
+	
+	public static void main(String[] args) {
+		
+		ContactBook cb = new ContactBook();
+		Contact erik = new Contact("Erik", "erblida@gmail.com", "070 497 57 59");
+		cb.addContactToList(erik);
+		
+		
+	}
+	
 }
