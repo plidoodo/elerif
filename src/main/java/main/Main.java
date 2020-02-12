0a5550d701f1469f71471312139e121a4601dde4
Elske <e.muijderman@gmail.com>
2020-02-12 23:31:23 +0100

    korigerade to string metoden

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -13,29 +13,35 @@ import ui.UiTva;
 public class Main {
 
 	public static void main(String[] args) throws ContactNotFoundException, IOException {
-		Contact c = new Contact();
-		ContactBook cl = new ContactBook();
-		cl.addContactToList(c.createContact("någon annan", "", "+31 63922880"));
-		cl.addContactToList(c.createContact("Aoife", "aoife@mejl.se", "+46 123 456 78"));
-		cl.addContactToList(c.createContact("Elske", "elske@mejl.se", "073 455 67 88"));
-		try {
-			System.out.println(cl.findContact("elske").toString());
-		} catch (Exception e) {
-			throw new ContactNotFoundException("Kontakt finns inte");
-			
-			
-		}
-	
-
-		UiTva ui = new UiTva();
-		
-		ui.userInterface();
-		
+//		Contact c = new Contact();
+//		ContactBook cl = new ContactBook();
+//		cl.addContactToList(c.createContact("någon annan", "", "+31 63922880"));
+//		cl.addContactToList(c.createContact("Aoife", "aoife@mejl.se", "+46 123 456 78"));
+//		cl.addContactToList(c.createContact("Elske", "elske@mejl.se", "073 455 67 88"));
+//		try {
+//			System.out.println(cl.findContact("elske").toString());
+//		} catch (Exception e) {
+//			throw new ContactNotFoundException("Kontakt finns inte");
+//			
+//			
+//		}
+//	
+//
+//		UiTva ui = new UiTva();
+//		
+//		ui.userInterface();
+//		
 //		Lagring l = new Lagring();
 //		l.addContactToFile(cl);
-		
-		
-		
+//		
+//		
+		ContactBook n = new ContactBook();
+		Lasning l = new Lasning();
+		l.loadAllContactsFromFile(n);
+		for(Contact next: n.getContacts()) {
+			System.out.println(next.toString());
+			System.out.println();
+		}
 
 		
 	}
