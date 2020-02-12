90fc3ec04e46c415af10c7b199dea378d13d8c31
Elske <e.muijderman@gmail.com>
2020-02-12 21:19:45 +0100

    lade till sista tester till ContactBookTest klassen

--- src/test/java/com/yrgo/contactbook/ContactBookTest.java
+++ src/test/java/com/yrgo/contactbook/ContactBookTest.java
@@ -13,6 +13,7 @@ public class ContactBookTest {
 
 	private Contact c;
 	private Contact d;
+	private Contact e;
 	private ContactBook cl;
 
 	// Test för att se om metoden addContact fungerar bra
@@ -74,6 +75,8 @@ public class ContactBookTest {
 		});
 	}
 
+	// Test för att kontrolera om kontakten verkligen blir uppdaterad med
+	// uppdateContact metoden
 	@Test
 	public void updateContactTest() throws Exception {
 		cl = new ContactBook();
@@ -85,10 +88,13 @@ public class ContactBookTest {
 		d.createContact("Sara", "sara@mejl.se", "073 450 67 99");
 		cl.updateContact(c, d);
 
-		boolean updated = cl.findContact("Sara").getMejl().equals("sara@mejl.se") && cl.findContact("Sara").getTelefonNr().equals("073 450 67 99");
+		boolean updated = cl.findContact("Sara").getMejl().equals("sara@mejl.se")
+				&& cl.findContact("Sara").getTelefonNr().equals("073 450 67 99");
 		assertTrue(updated, "Kontakt blev uppdaterat med korrekta mejl och telefonnummer");
 	}
-	
+
+	// Test för att kontrolera om kontakten blir borttagen från ContactBook med
+	// metoden deleteContact.
 	@Test
 	public void deleteContactTest() {
 		cl = new ContactBook();
@@ -96,12 +102,36 @@ public class ContactBookTest {
 		c = new Contact("Sara", "saar@mejl.se", "073 450 67 89");
 		cl.addContactToList(c.createContact("Sara", "saar@mejl.se", "073 450 67 89"));
 		int afterAdd = cl.numberOfContacts();
-		
+
 		cl.deleteContact(c);
 		int afterDelete = cl.numberOfContacts();
-		
-		boolean deleted = afterAdd == afterDelete +1;
+
+		boolean deleted = afterAdd == afterDelete + 1;
 		assertTrue(deleted, "Kontakt blev borttagen från ContactBook");
 	}
 
+	// Test för att kontrolera om sorteringsmetod fungerar bra. ContactBook blir
+	// sorterad varje gång man lägger till ett kontakt.
+	@Test
+	public void sortContactsTest() {
+		cl = new ContactBook();
+
+		c = new Contact("Sara", "saar@mejl.se", "073 450 67 89");
+		cl.addContactToList(c.createContact("Sara", "saar@mejl.se", "073 450 67 89"));
+
+		d = new Contact("Anna", "anna@mejl.se", "+46 731 23 45 67");
+		cl.addContactToList(d.createContact("Anna", "anna@mejl.se", "+46 731 23 45 67"));
+		
+		e = new Contact("Chris", "chris@mejl.se", "073 678 45 65");
+		cl.addContactToList(e.createContact("Chris", "chris@mejl.se", "073 678 45 65"));
+		
+		int positionSara = cl.findPosition(c);
+		int positionAnna = cl.findPosition(d);
+		int positionChris = cl.findPosition(e);
+		
+		boolean sorted = positionSara == 2 && positionAnna == 0 && positionChris == 1;
+		assertTrue(sorted, "Kontakter ligger på alfabetiskt följd i ContactBook");
+		
+		
+	}
 }
