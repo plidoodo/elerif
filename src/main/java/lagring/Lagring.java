35ba6f2e360524ea1e79eb4c6dd59f1af0a80a16
plidoodo <blixtdackhammar@gmail.com>
2020-02-14 14:23:41 +0100

    tog bort gamla metoder

--- src/main/java/lagring/Lagring.java
+++ src/main/java/lagring/Lagring.java
@@ -15,22 +15,7 @@ import doman.ContactBook;
 public class Lagring {
 	
 
-	public void addContactToFile(ContactBook cb) {
-
-		File file = new File("src\\main\\resources\\contacts.txt");
-		try  {
-			PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter(file)));
-			for (int i = 0; i < cb.getContacts().size(); i++) {
-				pw.println(cb.getContacts().get(i).toString());
-				pw.println();
-			}
-			pw.close();
-		} catch (IOException e) {
-
-			e.printStackTrace();
-		}
-
-	}
+	
 	
 	public void addToFile(ContactBook cb) {
 		
