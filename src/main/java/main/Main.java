85fb5eb6f792c1c9be7279ccc373478810a91bae
Elske <e.muijderman@gmail.com>
2020-02-17 10:05:28 +0100

    tog bort onodigt kod från main metoden

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -7,52 +7,10 @@ import ui.UiTva;
 public class Main {
 
 	public static void main(String[] args) {
-//		Contact c = new Contact();
-//		ContactBook cl = new ContactBook();
-//		cl.addContactToList(c.createContact("någon annan", "", "+31 63922880"));
-//		cl.addContactToList(c.createContact("Aoife", "aoife@mejl.se", "+46 123 456 78"));
-//		cl.addContactToList(c.createContact("Elske", "elske@mejl.se", "073 455 67 88"));
-//		try {
-//			System.out.println(cl.findContact("elske").toString());
-//		} catch (Exception e) {
-//			throw new ContactNotFoundException("Kontakt finns inte");
-//			
-//			
-//		}
-//	
-//
-//		UiTva ui = new UiTva();
-//		
-//		ui.userInterface();
-//		
-//		Lagring l = new Lagring();
-//		l.addContactToFile(cl);
-//		
-//		
-
-//		n.addContactToList(new Contact("Erik", "kalaspuff@mejl.se", "+31 63922880"));
-//		n.addContactToList(new Contact("Aoife", "aoife@mejl.se", "+46 123 456 78"));
-//		n.addContactToList(new Contact("Elske", "elske@mejl.se", "073 455 67 88"));
-//		Lagring lg = new Lagring();
-//		lg.addToFile(n);
-//		n.deleteContact(n.findContact("Erik"));
-//		n.deleteContact(n.findContact("Aoife"));
-//		n.deleteContact(n.findContact("Elske"));
-		ContactBook cb = new ContactBook();
-		for (Contact next: cb.getContacts()) {
-			System.out.println(next.toString());
-			System.out.println();
 			
 			UiTva ui = new UiTva();
 			
 			ui.userInterface();
-		}
+		}	
 		
-		
-		
-		
-		
-		
-
 	}
-}
